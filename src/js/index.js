dfcab2168aebd98032ffa83e9e49ebe781dbc270
EdgarDMen <123202799+EdgarDMen@users.noreply.github.com>
2023-05-10 21:20:25 +0000

    toDoList

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,58 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from 'react';
+import 'index.css';
+import Home from './component/home';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
 
-//import your own components
-import Home from "./component/home.jsx";
+  const addTask = () => {
+    if (input.trim() !== '') {
+      setTasks([...tasks, input]);
+      setInput('');
+    }
+  };
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, taskIndex) => taskIndex !== index);
+    setTasks(updatedTasks);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      addTask();
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.length === 0 ? (
+          <li>No tasks, add a task</li>
+        ) : (
+          tasks.map((task, index) => (
+            <li key={index}>
+              {task}{' '}
+              <span
+                style={{ cursor: 'pointer' }}
+                onClick={() => deleteTask(index)}
+              >
+                Trash
+              </span>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
